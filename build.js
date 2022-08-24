0885dfbf16c62f598609fd1161af28b7088772ba
Niels Roesen Abildgaard <niels.abildgaard@gmail.com>
2022-08-24 21:45:44 +0200

    prepublish script builds minified code
    Turned pre-example.js into a more generalized build.js which lets us build the minified version of the code.
    This can be used to build for the example when running that, but also building a distributable on publish.

---
+++ build.js
@@ -0,0 +1,16 @@
+const owbslib = require("./index");
+const fs = require("fs");
+const path = require("path");
+const mkdirp = require("mkdirp");
+const argv = require("yargs").argv;
+
+(async function() {
+    const code = await owbslib.generate();
+    const outputPath = path.join(__dirname, argv.output || "dist/owbs.min.js");
+    const outputFolder = path.dirname(outputPath);
+    await mkdirp(outputFolder);
+    fs.writeFileSync(outputPath, code);
+})()
+.catch((error) => {
+    console.error("failed", error);
+});
